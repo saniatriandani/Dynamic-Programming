95a4cc7acbfb4b504c9cff6a42d84f8cb7afa011
LENOVO <LENOVO@LENOVO-PC>
2018-09-12 13:16:51 +0700

    Initial Commit

---
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -0,0 +1,20 @@
+package DynamicProgramming;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+    
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("l, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+    
+    public static void main(String[] args) {
+        String identitas = "Sania tri andani / X RPL 3 / 35";
+        
+        tampilJudul(identitas);
+    }
+    
+}
