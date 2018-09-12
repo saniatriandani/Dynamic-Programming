4e7e1d6da38665c11148baee1345186826b15ce5
LENOVO <LENOVO@LENOVO-PC>
2018-09-12 13:25:01 +0700

    Initial Commit

--- src/DynamicProgramming/HitungFibonacci.java
+++ src/DynamicProgramming/HitungFibonacci.java
@@ -10,11 +10,22 @@ public class HitungFibonacci {
         System.out.println("\nHitung Fibonacci");
         System.out.println("l, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
     }
-    
+     private static int tampilInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan ke-: ");
+        int n = scanner.nextInt();
+        
+        return n;
+    }
     public static void main(String[] args) {
         String identitas = "Sania tri andani / X RPL 3 / 35";
         
         tampilJudul(identitas);
+        
+        int n = tampilInput();
+        
     }
     
 }
